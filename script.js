291bd48badd7cdcefeeba8febe42177c7a69d9b9
johnxwalker <djphantomgamer@gmail.com>
2025-11-15 18:10:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function home(){
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas SI-3X";
+}
+
+function mahasiswa () {
+    document.getElementById('selamat').innerHTML="Selamat Datang, Jimmy Vinargo";
+}
+
+function kelas() {
+    document.getElementById('selamat').innerHTML="Selamat Datang, Jimmy Vinargo SI-3D";
+}
+
+function singout(){
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas";
+}
